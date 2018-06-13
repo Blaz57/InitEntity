44d29e9a85330cb2022ed554c3a13cbed0c53d2d
CRM <CRM@CRM-UC-3624>
2018-06-13 10:47:31 +0200

    Inital Entity

---
+++ InitEntity/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InitEntity.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["nom"] = "Blaz";
+            ViewBag.Age = 35;
+            return View();
+        }
+    }
+}
